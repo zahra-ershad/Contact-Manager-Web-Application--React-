be5ac3d26e9f68367e04b44c7d0d41d761bc1b75
zahra Ershad <zahraershad.dev@gmail.com>
2026-01-15 12:32:55 +0330

    replaced useFromik by Formik and refactored addcontact form structure

--- src/components/Contacts/AddContact.jsx
+++ src/components/Contacts/AddContact.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
-import {useFormik} from 'formik';
+import {Formik} from 'formik';
 import Spinner from '../Spinner';
 import Register from '../../assets/register.gif';
 import {} from '../../helpers/colors';
@@ -20,24 +20,11 @@ const AddContact = () => {
           } = useContext(ContactContext);
 
 
-          const formik = useFormik({
+         
 
-            initialValues:{
-              fullName:'' ,
-              photo:'' ,
-              mobile:'' ,
-              email: '',
-              job:'' ,
-              group:'' ,
-            } ,
-            validationSchema : contactSchema ,
+           
 
-            onSubmit : (values) =>{
-              console.log(values);
-              creatContact(values);
-            }
-
-          });
+         
 
 
     return (
@@ -67,124 +54,147 @@ const AddContact = () => {
                   
                   <div className="row justify-content-center">
                     <div className="col-12 col-md-8 col-lg-6">
-                      <form className="contact-form" onSubmit={formik.handleSubmit}>
-                        <div className="form-group">
-                          <input 
-                            type="text" 
-                            id="fullName"
-                            {...formik.getFieldProps("fullname")}
-                            placeholder="نام و نام خانوداگی" 
-                            className="form-input"
-                             
-                          />
-                        </div>
-                        {
-                          (formik.touched.fullName&& formik.errors.fullName ) ?
+                     <Formik
 
-                                  <p className="errMsg">{formik.errors.fullName}</p> 
-                                  : null
-                        }
-                        
-                        <div className="form-group">
-                          <input 
-                            type="text" 
-                            id="photo"
-                            {...formik.getFieldProps('photo')} 
-                            className="form-input"
-                            placeholder="آدرس عکس"
-                             
-                          />
-                        </div>
-                        {
-                          (formik.errors.photo && formik.touched.photo) ?
+                      initialValues={{
+                        fullName:'' ,
+                        photo:'' ,
+                        mobile:'' ,
+                        email: '',
+                        job:'' ,
+                        group:'' ,
+                      } }
+                      validationSchema ={contactSchema}
+          
+                      onSubmit ={ (values) =>{
+                        creatContact={values}
+                      }}
+                     >
+                      {
+                        (formik)=> (
+                          <form className="contact-form" onSubmit={formik.handleSubmit}>
+                          <div className="form-group">
+                            <input 
+                              type="text" 
+                              id="fullName"
+                              {...formik.getFieldProps("fullname")}
+                              placeholder="نام و نام خانوداگی" 
+                              className="form-input"
+                               
+                            />
+                          </div>
+                          {
+                            (formik.touched.fullName&& formik.errors.fullName ) ?
+  
+                                    <p className="errMsg">{formik.errors.fullName}</p> 
+                                    : null
+                          }
                           
-                                  <p className="errMsg">{formik.errors.photo}</p> 
-                                  : null
-                        }
-                        
-                        <div className="form-group">
-                          <input 
-                            type="number" 
-                            id="mobile"
-                            {...formik.getFieldProps('mobile')}
-                            placeholder="شماره موبایل" 
-                            className="form-input"
-                             
-                          />
-                        </div>
-                        {
-                          (formik.errors.mobile && formik.touched.mobile) ?
+                          <div className="form-group">
+                            <input 
+                              type="text" 
+                              id="photo"
+                              {...formik.getFieldProps('photo')} 
+                              className="form-input"
+                              placeholder="آدرس عکس"
+                               
+                            />
+                          </div>
+                          {
+                            (formik.errors.photo && formik.touched.photo) ?
+                            
+                                    <p className="errMsg">{formik.errors.photo}</p> 
+                                    : null
+                          }
                           
-                                  <p className="errMsg">{formik.errors.mobile}</p> 
-                                  : null
-                        }
-                        
-                        <div className="form-group">
-                          <input 
-                            type="email" 
-                            id="email" 
-                           {...formik.getFieldProps('email')}
-                            placeholder="آدرس ایمیل" 
-                            className="form-input"
-                             
-                          />
-                        </div>
-                        {
-                          (formik.errors.email && formik.touched.email) ?
+                          <div className="form-group">
+                            <input 
+                              type="number" 
+                              id="mobile"
+                              {...formik.getFieldProps('mobile')}
+                              placeholder="شماره موبایل" 
+                              className="form-input"
+                               
+                            />
+                          </div>
+                          {
+                            (formik.errors.mobile && formik.touched.mobile) ?
+                            
+                                    <p className="errMsg">{formik.errors.mobile}</p> 
+                                    : null
+                          }
                           
-                                  <p className="errMsg">{formik.errors.email}</p> 
-                                  : null
-                        }
-                        
-                        <div className="form-group">
-                          <input 
-                            type="text" 
-                            id="job" 
-                            {...formik.getFieldProps('job')}
-                            placeholder="شغل" 
-                            className="form-input"
-                             
-                          />
-                        </div>
-                        {
-                          (formik.errors.job && formik.touched.job) ?
+                          <div className="form-group">
+                            <input 
+                              type="email" 
+                              id="email" 
+                             {...formik.getFieldProps('email')}
+                              placeholder="آدرس ایمیل" 
+                              className="form-input"
+                               
+                            />
+                          </div>
+                          {
+                            (formik.errors.email && formik.touched.email) ?
+                            
+                                    <p className="errMsg">{formik.errors.email}</p> 
+                                    : null
+                          }
                           
-                                  <p className="errMsg">{formik.errors.job}</p> 
-                                  : null
-                        }
-                        
-                        <div className="form-group">
-                          <select 
-                          id="group"
-                          {...formik.getFieldProps('group')}
-                          className="form-select">
-                            {
-                                groups.length > 0 && groups.map(group =>(
-                                    <option value={group.id} key={group.id}>
-                                        {group.name }
-                                    </option>
-                                ))
-                            }
-                          </select>
-                        </div>
-                        {
-                          (formik.errors.group && formik.touched.group) ?
+                          <div className="form-group">
+                            <input 
+                              type="text" 
+                              id="job" 
+                              {...formik.getFieldProps('job')}
+                              placeholder="شغل" 
+                              className="form-input"
+                               
+                            />
+                          </div>
+                          {
+                            (formik.errors.job && formik.touched.job) ?
+                            
+                                    <p className="errMsg">{formik.errors.job}</p> 
+                                    : null
+                          }
                           
-                                  <p className="errMsg">{formik.errors.group}</p> 
-                                  : null
-                        }
-                        
-                        <div className="form-actions">
-                          <input 
-                            type="submit" 
-                            value="ایجاد مخاطب" 
-                            className="submit-btn"
-                          />
-                          <Link to={'/contacts'} className="back-btn">
-                            بازگشت به لیست
-                          </Link>
-                        </div>
-                      </form>
+                          <div className="form-group">
+                            <select 
+                            id="group"
+                            {...formik.getFieldProps('group')}
+                            className="form-select">
+                              {
+                                  groups.length > 0 && groups.map(group =>(
+                                      <option value={group.id} key={group.id}>
+                                          {group.name }
+                                      </option>
+                                  ))
+                              }
+                            </select>
+                          </div>
+                          {
+                            (formik.errors.group && formik.touched.group) ?
+                            
+                                    <p className="errMsg">{formik.errors.group}</p> 
+                                    : null
+                          }
+                          
+                          <div className="form-actions">
+                            <input 
+                              type="submit" 
+                              value="ایجاد مخاطب" 
+                              className="submit-btn"
+                            />
+                            <Link to={'/contacts'} className="back-btn">
+                              بازگشت به لیست
+                            </Link>
+                          </div>
+                        </form>
+                        )
+                      }
+
+                     </Formik>
+                     
                     </div>
                   </div>
                   <br />
